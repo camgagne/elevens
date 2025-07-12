c922f69dc8ab0756ed2ea01362ff0a33a015a04d
Cam Gagne <gagne.cam.17@gmail.com>
2025-07-11 21:01:41 -0400

    Add files via upload

---
+++ DeckTester.java
@@ -0,0 +1,17 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *    @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "jack", "queen", "king"};
+        String[] suits = {"Hearts" , "Spades", "Clubs", "Dimonds"};
+        int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
+        System.out.println(d);
+    }
+}
